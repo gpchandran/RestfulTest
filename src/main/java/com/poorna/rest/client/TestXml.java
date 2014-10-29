b458b69cbd3b016fe049928c07a35d72e7e6f8a1
pg1000 <poorna.ganesan@optum.com>
2014-10-29 11:36:18 -0500

    dev branch check-in

---
+++ src/main/java/com/poorna/rest/client/TestXml.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.poorna.rest.client;
+
+/**
+ * @author pg1000
+ *
+ */
+public class TestXml {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
